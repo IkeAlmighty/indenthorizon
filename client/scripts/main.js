510ea644c5703f117133a9643f2e4fb1558095cf
ikealmighty <isaacyates7@gmail.com>
2025-12-16 20:37:54 -0600

    refractor

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,8 +1,11 @@
 /**
  *
- * @param {typeof import("../functions.js")} api - represents all the possible ways an entity (like a
+ * @param {typeof import("../api.js")} api - represents all the possible ways an entity (like a
  * ship) can interact with the game.
  */
 export default async function loop(api) {
   //  your code here
+
+  const status = await api.getStatus();
+  // console.log(status);
 }
